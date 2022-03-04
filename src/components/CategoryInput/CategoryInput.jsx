37f1cf5f86a028529d8d6743dd89bb21d3736fc4
Toivo Hannigan <THannigan046@gmail.com>
2022-03-04 16:39:09 -0600

    added dropdown

--- src/components/CategoryInput/CategoryInput.jsx
+++ src/components/CategoryInput/CategoryInput.jsx
@@ -2,25 +2,31 @@ import * as React from 'react';
 
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 function CategoryInput() {
 // homes boats and rvs
-// is in figma 
+// is in figma, not in scope doc 
 
     return (
         <>
             <h1>this is the category input thing</h1>
-            <Box
-                component="form"
-                sx={{
-                    '& > :not(style)': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-            >
-                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-                <TextField id="filled-basic" label="Filled" variant="filled" />
-                <TextField id="standard-basic" label="Standard" variant="standard" />
+            <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Category</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        label="Age"
+                    >
+                        <MenuItem value={'Homes'}>Homes</MenuItem>
+                        <MenuItem value={'Boats'}>Boats</MenuItem>
+                        <MenuItem value={'Rvs'}>Rvs</MenuItem>
+                    </Select>
+                </FormControl>
             </Box>
 
         </>
