5b3ce56e19ad2858e3f7bd086c86ef2be703e1e6
Toivo Hannigan <THannigan046@gmail.com>
2022-03-04 15:25:27 -0600

    initial commit

--- database.sql
+++ database.sql
@@ -4,7 +4,52 @@
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+  "id" SERIAL PRIMARY KEY,
+  "firstName" VARCHAR(256),
+  "lasrName" VARCHAR(256),
+  "username" VARCHAR (256) UNIQUE NOT NULL,
+  "password" VARCHAR (2550) NOT NULL,
+  "email" VARCHAR(256),
+  "phoneNumber" VARCHAR(256),
+  "companyName" VARCHAR(256),
+  "address" VARCHAR(2560),
+  "city"	VARCHAR(256),
+  "state" VARCHAR(256),
+  "zipcode" VARCHAR(30),
+  "websiteUrl" VARCHAR(2560),
+  "authLevel" VARCHAR(256) DEFAULT'renter'
+);
+CREATE TABLE "categories" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(2500),
+	"parentId" INTEGER,
+	FOREIGN KEY ("parentId") REFERENCES "categories"(id)
+);
+CREATE TABLE "bookable_items" (
+	"id" SERIAL PRIMARY KEY,
+	"title" VARCHAR(2500),
+	"summary" VARCHAR(2560),
+	"detail" VARCHAR(2560),
+	"rate" INTEGER,
+	"unitTime" VARCHAR(2500),
+	"location" VARCHAR(2500),
+	"categoryId" INTEGER,
+	FOREIGN KEY ("categoryId") REFERENCES "categories"(id),
+	"clientId" INTEGER,
+	FOREIGN KEY ("clientId") REFERENCES "user"(id)
+);
+CREATE TABLE "photos" (
+	"id" SERIAL PRIMARY KEY,
+	"url" VARCHAR(2500),
+	"itemId" INTEGER,
+	FOREIGN KEY ("itemId") REFERENCES "bookable_items"(id)
+);
+CREATE TABLE "renter_booking" (
+	"id" SERIAL PRIMARY KEY,
+	"startDate" DATE,
+	"hours_book" INTEGER,
+	"renterId" INTEGER,
+	FOREIGN KEY ("renterId") REFERENCES "user"(id),
+	"bookableId" INTEGER,
+	FOREIGN KEY ("bookableId") REFERENCES "bookable_items"(id)
 );
