77cfd03eae7933236067037ea2ff1c13448a4bdb
Leah Grim <grimchelate@gmail.com>
2022-03-05 21:48:43 -0600

    about page updated

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './AboutPage.css'
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -8,9 +9,51 @@ import React from 'react';
 function AboutPage() {
   return (
     <div className="container">
-      <div>
-        <p>This about page is for anyone to read!</p>
+    <div className="infoBoxList">
+          <div className="infoTitle"> 
+            <h2 className="listInfoHeading"> Technologies Implemented </h2>
+            <h3 className="listItem" > • React js / Material UI</h3>
+            <h3 className="listItem" > • Node js  / PostgreSQL  </h3>
+            <h3 className="listItem" > • Javascript / Html / CSS </h3>
+            
+          <img 
+                src= {'https://www.upsnowmobiling.com/images/Snowmobile_Rentals.jpg'}
+                width= {500}
+                height={420}
+                className="OwlPic"
+            />
+          </div>
+      
+      <div className="infoTitle"> 
+        <h2 className="infoHeading"> Eazie-Book</h2>
+        <h3 className="topBorder" > An app built to simplify booking recreational vehicles and leisure boats for small businesses </h3>
+        <h3 className="listItem" > </h3>
+        <h3 className="listItem" > </h3>
+      <img 
+            src= {'https://img.grouponcdn.com/deal/tvs3vCXXs1Pc9UYqESzHXvMFM6F/tv-1164x874/v1/c870x524.jpg'}
+            width= {700}
+            height={400}
+            className="OwlPic"
+        />
       </div>
+        
+          <div className="infoTitle"> 
+            <h2 className="listInfoHeading"> Looking to List Your Rental Equipment on Eazie-Book? </h2>
+         
+  
+            <h3 className="listItem" > Contact Dean via email at Dean@calldeans.com  </h3>
+          <img 
+                src= {'https://www.destinvacationboatrentals.com/wp-content/uploads/2013/04/IMG_3081.jpg'}
+                width= {500}
+                height={450}
+                className="OwlPic"
+            />
+          </div>
+          </div>
+          <div className="footerInfo">
+            <h3> App Creators :: Ken Dang, Jonathan Normand, Leah Grim, Toivo Hannigan</h3>
+           
+          </div>
     </div>
   );
 }
