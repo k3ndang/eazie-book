8c8adeff0d4b9e47ae5c612bd26fff986bb7c90a
Leah Grim <grimchelate@gmail.com>
2022-03-07 14:55:23 -0600

    client list styling changes

--- src/components/ClientTable/ClientTable.jsx
+++ src/components/ClientTable/ClientTable.jsx
@@ -3,8 +3,6 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-
-
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -13,10 +11,10 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
-
+import './ClientTable.css'
 function ClientTable(){
-
-    const dispatch = useDispatch();
+        const history = useHistory();
+        const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch({
@@ -24,43 +22,53 @@ function ClientTable(){
         })
     }, [])
 
-    const clients = useSelector(store => store.clients);
-
+        const clients = useSelector(store => store.clients);
+        
+        const addClient = () => {
+            history.push("/admin/invite");
+        }
     return(
         <>
         <div className="clientTable">
+            <div className="clientListButton" >
+                <h2>  Current list of Clients </h2>
+                <button className="addClientButton" onClick={addClient}> Add Client </button>
+
+            </div>
+            
+            <h3 className="adminTableTitle"> Client Information</h3>
             <TableContainer component={Paper}>
             <Table>
                 <TableHead>
-                    <TableRow>
-                        <TableCell>First Name</TableCell>
-                        <TableCell align="right">Last Name</TableCell>
-                        <TableCell align="right">Username</TableCell>
-                        <TableCell align="right">Email</TableCell>
-                        <TableCell align="right">Phone Number</TableCell>
-                        <TableCell align="right">Company Name</TableCell>
-                        <TableCell align="right">Address</TableCell>
-                        <TableCell align="right">City</TableCell>
-                        <TableCell align="right">State</TableCell>
-                        <TableCell align="right">Zip Code</TableCell>
-                        <TableCell align="right">Website URL</TableCell>
-                        <TableCell align="right">Bookable Items</TableCell>
+                    <TableRow >
+                        <TableCell align="center" className="clientListTableHeadings">First Name</TableCell>
+                        <TableCell align="center" className="clientListTableHeadings">Last Name</TableCell>
+                        <TableCell align="center" className="clientListTableHeadings">Username</TableCell>
+                        <TableCell align="center" className="clientListTableHeadings">Email</TableCell>
+                        <TableCell align="center" className="clientListTableHeadings">Phone Number</TableCell>
+                        <TableCell align="center" className="clientListTableHeadings">Company Name</TableCell>
+                        <TableCell align="center" className="clientListTableHeadings">Address</TableCell>
+                        <TableCell align="center" className="clientListTableHeadings">City</TableCell>
+                        <TableCell align="center" className="clientListTableHeadings">State</TableCell>
+                        <TableCell align="center" className="clientListTableHeadings">Zip Code</TableCell>
+                        <TableCell align="center" className="clientListTableHeadings">Website URL</TableCell>
+                        <TableCell align="center" className="clientListTableHeadings">Bookable Items</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {clients.map(client => (
                         <TableRow key={client.id} id={client.id}>
-                            <TableCell>{client.firstName}</TableCell>
-                            <TableCell align="right">{client.lastName}</TableCell>
-                            <TableCell align="right">{client.username}</TableCell>
-                            <TableCell align="right">{client.email}</TableCell>
-                            <TableCell align="right">{client.phoneNumber}</TableCell>
-                            <TableCell align="right">{client.companyName}</TableCell>
-                            <TableCell align="right">{client.address}</TableCell>
-                            <TableCell align="right">{client.city}</TableCell>
-                            <TableCell align="right">{client.state}</TableCell>
-                            <TableCell align="right">{client.zipcode}</TableCell>
-                            <TableCell align="right">{client.websiteUrl}</TableCell>
+                            <TableCell align="center" className="clientListTableRow">{client.firstName}</TableCell>
+                            <TableCell align="center" className="clientListTableRow">{client.lastName}</TableCell>
+                            <TableCell align="center" className="clientListTableRow">{client.username}</TableCell>
+                            <TableCell align="center" className="clientListTableRow">{client.email}</TableCell>
+                            <TableCell align="center" className="clientListTableRow">{client.phoneNumber}</TableCell>
+                            <TableCell align="center" className="clientListTableRow">{client.companyName}</TableCell>
+                            <TableCell align="center" className="clientListTableRow">{client.address}</TableCell>
+                            <TableCell align="center" className="clientListTableRow">{client.city}</TableCell>
+                            <TableCell align="center" className="clientListTableRow">{client.state}</TableCell>
+                            <TableCell align="center" className="clientListTableRow">{client.zipcode}</TableCell>
+                            <TableCell align="center" className="clientListTableRow">{client.websiteUrl}</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
