ea660b1ddc7f4697cc3379b63737a86fe408a9ed
Toivo Hannigan <THannigan046@gmail.com>
2022-03-09 10:55:45 -0600

    abandoned imgList

--- src/components/ClientBookableItems/ClientBookableItems.jsx
+++ src/components/ClientBookableItems/ClientBookableItems.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
-
+import './ClientBookableItem.css'
 const itemData = [
     {
         img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
@@ -71,23 +71,19 @@ function ClientBookableItems() {
         <>
 
             <h1>Client bookable items thing</h1>
-            <ImageList sx={{ width: 800, height: 750 } }>
-                {itemData.map((item) => (
-                    <ImageListItem key={item.img}>
+            <section className="clientBookableItems">
+                {itemData.map(item => {
+                    return(
+                        <>
                         <img
-                            src={`${item.img}?w=248&fit=crop&auto=format`}
-                            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                            alt={item.title}
-                            loading="lazy"
+                            src={item.img}
+
                         />
-                        <ImageListItemBar
-                            title={item.title}
-                            subtitle={<span>by: {item.author}</span>}
-                            position="below"
-                        />
-                    </ImageListItem>
-                ))}
-            </ImageList>
+                        <h2>{item.title}</h2>
+                        </>
+                    )
+                })}
+            </section>
         </>
     )
 }
