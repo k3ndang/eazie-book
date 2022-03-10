7c82c30cd44e570d1235bbc0db6fc41e71bf4bbf
Leah Grim <grimchelate@gmail.com>
2022-03-10 12:47:58 -0600

    fixed error messages on get requests in client detail router

--- server/routes/clientDetailItem.router.js
+++ server/routes/clientDetailItem.router.js
@@ -29,7 +29,7 @@ router.get('/', rejectUnauthenticated, (req, res) => {
         res.send(result.rows);
     })
     .catch((err) => {
-        console.error('ERROR getting clients in category.reducer', err);
+        console.error('ERROR getting bookable items according to client id in clientDetailItem.router', err);
         res.sendStatus(500);
     })
 }) 
@@ -39,29 +39,29 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 // Router to grab the specific bookable item on the client detail page 
 router.get('/:id', rejectUnauthenticated, (req, res) => {
     const queryText = ` 
-    SELECT 
-    "bookable_items"."title", 
-    "bookable_items"."summary", 
-    "bookable_items"."detail", 
-    "bookable_items"."rate", 
-    "bookable_items"."categoryId",
-    "bookable_items"."unitTime", 
-    "bookable_items"."location",
-    "categories"."name", 
-    "photos"."url"
-    FROM "bookable_items"
-    JOIN "categories" ON "categories"."id"="bookable_items"."categoryId"
-    JOIN "photos" ON "photos"."itemId"="bookable_items"."id"  
-    JOIN "user" ON "user".id="bookable_items"."clientId" 
-    WHERE "bookable_items"."id"= $1 AND "user".id= $2 ;
-    `;
+                        SELECT 
+                        "bookable_items"."title", 
+                        "bookable_items"."summary", 
+                        "bookable_items"."detail", 
+                        "bookable_items"."rate", 
+                        "bookable_items"."categoryId",
+                        "bookable_items"."unitTime", 
+                        "bookable_items"."location",
+                        "categories"."name", 
+                        "photos"."url"
+                        FROM "bookable_items"
+                        JOIN "categories" ON "categories"."id"="bookable_items"."categoryId"
+                        JOIN "photos" ON "photos"."itemId"="bookable_items"."id"  
+                        JOIN "user" ON "user".id="bookable_items"."clientId" 
+                        WHERE "bookable_items"."id"= $1 AND "user".id= $2 ;
+                        `;
     const queryParams = [req.params.id, req.user.id]
     pool.query(queryText)
     .then((result) => {
         res.send(result.rows);
     })
     .catch((err) => {
-        console.error('ERROR getting clients in category.reducer', err);
+        console.error('ERROR in get/:id getting bookable item detail data in clientDetailItem.router', err);
         res.sendStatus(500);
     })
 }) 
