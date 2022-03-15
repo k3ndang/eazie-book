2ad46b58e2a1580f22c65291668f74c31dd4ad75
Toivo Hannigan <THannigan046@gmail.com>
2022-03-15 10:39:18 -0500

    made the words bigger

--- src/components/AcctInfo/AcctInfo.jsx
+++ src/components/AcctInfo/AcctInfo.jsx
@@ -6,9 +6,9 @@ function AcctInfo() {
         <>
         <h1>Acct Info (user types) </h1>
         
-        <h3>Admin: as an admin, you will have control over the entire Eaziebook network. you'll be able to invite clients and add them to the database, assign categories to bookable items, and view complete lists of clients and bookable items, with the ability to edit or delete any of them. You can also add a photo to any given bookable item. </h3>
-        <h3>Client: as a client, you represent a company providing a service and using eaziebook as a middleman for said service. From your page you'll be able to view a list of your company's bookable items, as well as an info table for clients who've booked your items. </h3>
-        <h3>Renter: you get to have all the fun! As a renter, you can book any of the services eaziebook provides. Click on make a reservation to view categories, click on a category to your desired item type, select your item, pick your dates to book it, and you're set! You can also view all of your item reservations and see the info for the parent company you booked through. </h3>
+            <h2><h1>Admin:</h1> as an admin, you'll have control over the entire Eaziebook network. you'll be able to invite clients and add them to the database, assign categories to bookable items, and view complete lists of clients and bookable items, with the ability to edit or delete any of them. You can also add a photo to any given bookable item. </h2>
+            <h2><h1>Client:</h1> as a client, you represent a company providing a service and using eaziebook as a middleman for said service. From your page you'll be able to view a list of your company's bookable items, as well as an info table for clients who've booked your items. </h2>
+            <h2><h1>Renter: </h1>you get to have all the fun! As a renter, you can book any of the services eaziebook provides. Click on make a reservation to view categories, click on a category to your desired item type, select your item, pick your dates to book it, and you're set! You can also view all of your item reservations and see the info for the parent company you booked through. </h2>
         </>
     )
 }
