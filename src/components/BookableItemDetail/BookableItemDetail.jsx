8cefc63f5e3cfdcaed9ca05e7253ef6f9c9a3975
Toivo Hannigan <THannigan046@gmail.com>
2022-03-16 16:33:47 -0500

    weird that my changes got lost

--- src/components/BookableItemDetail/BookableItemDetail.jsx
+++ src/components/BookableItemDetail/BookableItemDetail.jsx
@@ -66,45 +66,80 @@ function BookableItemDetail() {
 
     return (
         <>
-        <Grid>
-        <h2>Item Details</h2>
-        
-            {selectedItem.url?.map((photo, i) => (
-            <img
-                key={i}
-                src={photo}
-            />
-            ))}
+            <Grid container spacing={4} direction='column' alignItems='center'>
+                <h2>Item Details</h2>
+                <img
+                    src="https://www.amfam.com/-/media/images/amfam/products/boat/product-page-speed-and-power-boats---m.jpg"
+                    alt={selectedItem.title}
+                />
+                <Typography variant="h4">Title: {selectedItem.title}</Typography>
+                <Typography variant="h6">Summary: {selectedItem.summary}</Typography>
+                <Typography variant="h6">Detail: {selectedItem.detail}</Typography>
+                <Typography variant="h6">Rate: {selectedItem.rate}</Typography>
+                <Grid item>
+                    <h4>Click below to select date</h4>
+                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                        {/* <DatePicker value={selectedDate} onChange={handleDateChange} /> */}
+                        <DateTimePicker value={selectedDate} onChange={handleDateChange} />
+                    </MuiPickersUtilsProvider>
+                    <h4>Renting Hours</h4>
+                    <Autocomplete
+                        ListboxProps={{
+                            style: {
+                                position: "absolute",
+                                top: 10,
+                                bottom: 0,
+                                right: 0,
+                                left: 0,
+                                height: 300,
+                                width: "100%",
+                                overflowY: "auto",
+                                backgroundColor: "white",
+                            },
+                        }}
+                        options={hours}
+                        // sx={{ width: 350 }}
+                        value={hours.id}
+                        fontSize="20px"
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                variant="standard"
+                                label="Hours"
 
-        <Typography variant="h4">Title: {selectedItem.title}</Typography>
-        <Typography variant="h6">Summary: {selectedItem.summary}</Typography>
-        <Typography variant="h6">Detail: {selectedItem.detail}</Typography>
-        <Typography variant="h6">Rate: {selectedItem.rate}</Typography>
+                            />
+                        )}
+                        onChange={(event, newValue) => setHoursBook(newValue.id)}
+                    />
+                </Grid>
 
-        <h4>Select Date</h4>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                {/* <DatePicker value={selectedDate} onChange={handleDateChange} /> */}
-                <DateTimePicker value={selectedDate} onChange={handleDateChange} />
-            </MuiPickersUtilsProvider>
-        <h4>Renting Hours</h4>
-        <Autocomplete
-            options= {hours}
-            sx={{ width: 350 }}
-            value= {hours.id}
-            fontSize="20px"
-            renderInput={(params) => <TextField {...params} label="How Many Hours?" />}
-            onChange={(event, newValue) => setHoursBook(newValue.id)}
-            />
-            <Button 
-                type="submit"
-                variant="outlined"
-                color="primary"
-                size="small"
-                onClick={(evt) => bookingNow(evt)}
-            >
-                Book Now
-            </Button>
-        </Grid>
+                {/* <Grid item>
+                    <Select 
+                        onChange={(event, newValue) => setHoursBook(newValue.id)}
+                        name='hours' id='hours'
+                    >
+                        <MenuItem value="1">1</MenuItem>
+                        <MenuItem value="2">2</MenuItem>
+                        <MenuItem value="3">3</MenuItem>
+                        <MenuItem value="4">4</MenuItem>
+                        <MenuItem value="5">5</MenuItem>
+                        <MenuItem value="6">6</MenuItem>
+                        <MenuItem value="7">7</MenuItem>
+                        <MenuItem value="8">8</MenuItem>
+ 
+                    </Select>
+                </Grid> */}
+                <Button
+                    type="submit"
+                    variant="outlined"
+                    color="primary"
+                    size="small"
+                    onClick={(evt) => bookingNow(evt)}
+                >
+                    Book Now
+                </Button>
+
+            </Grid>
         </>
     )
 }
