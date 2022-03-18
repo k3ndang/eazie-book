6483df06390c9feab2064f0f0fa768f1895c9152
Leah Grim <grimchelate@gmail.com>
2022-03-18 09:26:10 -0500

    nav bar updated

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -72,7 +72,7 @@ function Nav() {
                 <Link to="/admin/invite"> Add a Client  </Link>
                 {/* bring admin to category form */}
                 {/* Could also say "edit bookable categories" */}
-                <Link to={`/addBookableItem/${user.id}`}> Add a Bookable Item </Link>
+                <Link to="/addBookableItem"> Add a Bookable Item </Link>
                 {/* brings admin to list of clients, perhaps this is redundant */}
                 {/* <Link to="/"> Edit Clients </Link> */}
               </div>
@@ -90,9 +90,9 @@ function Nav() {
             <div className="dropdown">
               <button className="dropbtn"> About</button>
               <div className="dropdown-content">
-                <Link to="/clientBookableItems"> About Page </Link>
+                <Link to="/about"> About Page </Link>
                 {/* This should be the list of everyone that has rented bookable items  */}
-                <Link to="/about"> Acct Info </Link>
+                <Link to="/acctInfo"> Acct Info </Link>
               </div>
             </div>
             {/* having an about and an info page seems redundant */}
